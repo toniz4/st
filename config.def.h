4e9cd9a7173cfcec201a488550f89720fb8dd5d0
Cássio Ribeiro Alves de Ávila <cassioavila@protonmail.com>
2020-10-13 01:11:03 -0300

    changed theme

--- config.def.h
+++ config.def.h
@@ -107,34 +107,34 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* solarized light */
-	"#073642",  /*  0: black    */
-	"#dc322f",  /*  1: red      */
-	"#859900",  /*  2: green    */
-	"#b58900",  /*  3: yellow   */
-	"#268bd2",  /*  4: blue     */
-	"#d33682",  /*  5: magenta  */
-	"#2aa198",  /*  6: cyan     */
-	"#eee8d5",  /*  7: white    */
-	"#002b36",  /*  8: brblack  */
-	"#cb4b16",  /*  9: brred    */
-	"#586e75",  /* 10: brgreen  */
-	"#657b83",  /* 11: bryellow */
-	"#839496",  /* 12: brblue   */
-	"#6c71c4",  /* 13: brmagenta*/
-	"#93a1a1",  /* 14: brcyan   */
-	"#fdf6e3",  /* 15: brwhite  */
+	/* 8 normal colors */
+	"#444444",
+	"#af0000",
+	"#008700",
+	"#5f8700",
+	"#0087af",
+	"#878787",
+	"#005f87",
+	"#eeeeee",
+	/* 8 bright colors */
+	"#005f87",
+	"#d70000",
+	"#d70087",
+	"#8700af",
+	"#d75f00",
+	"#d75f00",
+	"#005faf",
+	"#bcbcbc",
 };
-
-
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 11;
-unsigned int defaultbg = 15;
-static unsigned int defaultcs = 10;
-static unsigned int defaultrcs = 8;
+unsigned int defaultfg = 0;
+unsigned int defaultbg = 7;
+static unsigned int defaultcs = 0;
+static unsigned int defaultrcs = 6;
+
 
 /*
  * Default shape of cursor
@@ -213,6 +213,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_I,           iso14755,       {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ MODKEY,        	    XK_k,     		kscrollup,      {.i =  1} },
+	{ MODKEY,          	    XK_j,   		kscrolldown,    {.i =  1} },
 };
 
 /*
